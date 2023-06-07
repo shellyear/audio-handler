a97a9ebc0fa31e1cd12915af4471193e6dad54bf
Nurbek Yelesh <boolevafunkce@gmail.com>
2023-06-08 00:04:20 +0200

    fix: solved error 'cannot use import from outside a module' using commonJs syntax in eslint and prettier config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,32 @@
-import prettierConfig from './.prettierrc'
+const prettierConfig = require('./.prettierrc')
 
-export default {
-    env: {
-        "browser": true,
-        "es2021": true
-    },
-    extends: [ // extending my config with these configs and plugins. (config can include plugins)
-        'airbnb', 
-        'airbnb/hooks', 
-        'prettier',
-        'plugin:prettier/recommended',
-        "plugin:react-hooks/recommended" 
-    ],
-    parser: '@typescript-eslint/parser',
-    plugins: ['react', 'react-hooks', 'import', 'prettier'],
-    parserOptions: {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    rules: {
-        'prettier/prettier': ['error', prettierConfig]
-    },
-    settings: {
-        'import/resolver': {
-          node: {
-            extensions: ['.js', '.jsx', '.ts', '.tsx'],
-          },
-        },
-      },
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    // extending my config with these configs and plugins. (config can include plugins)
+    'airbnb',
+    'airbnb/hooks',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', 'react-hooks', 'import', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'prettier/prettier': ['error', prettierConfig]
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+      }
+    }
   }
+}
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
 	"printWidth": 120,
 	"tabWidth": 2,
 	"semi": false,
