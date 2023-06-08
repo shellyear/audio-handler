ed172f50a485ccf51952018d1eb280586f2195a4
Nurbek Yelesh <boolevafunkce@gmail.com>
2023-06-08 17:15:29 +0200

    feat: add tailwindcss with it's plugin for prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,9 @@
 module.exports = {
-	"printWidth": 120,
-	"tabWidth": 2,
-	"semi": false,
-	"trailingComma": "none",
-	"singleQuote": true,
-	"endOfLine": "auto"
+	printWidth: 120,
+	tabWidth: 2,
+	semi: false,
+	trailingComma: "none",
+	singleQuote: true,
+	endOfLine: "auto",
+	plugins: [require('prettier-plugin-tailwindcss')] // adding explicitly, but it auloloads by default by most package managers
 }
